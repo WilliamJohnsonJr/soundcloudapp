a2bf964cb1e72d80ecc27b445f046cc10a0daaad
Will Johnson <williamterryjohnsonjr@gmail.com>
2016-07-01 15:02:19 -0400

    Wrote AJAX calls in sc.js to return a user-submitted query, then used jQuery .on() in main.js to inject the results of the query as images, etc. in a grid in index.html

--- src/js/main.js
+++ src/js/main.js
@@ -1,9 +1,24 @@
 import $ from 'jquery';
 import _ from 'lodash';
 import {client_id} from './credentials';
-import {baseURL} from './sc';
+import {baseURL, musicSearch} from './sc';
 
+$(".searchButton").on('click', function (e) {
+	e.preventDefault();
+	var input = $(".searchInput").val();
+	var musicData = musicSearch(input);
 
-
-
-
+	musicData.then(function(musicData){
+		musicData.forEach(function(object){
+			$(".grid").append(`
+				<div class="trackContainer">
+					<div class="trackArtworkDiv">
+						<img class="artwork" src="${object.artwork_url}">
+					</div>
+					<div class="songTitle">${object.title}
+					</div>
+					<div class="bandName">${object.user.username}
+				</div>`);
+		});
+	});
+});
--- src/js/sc.js
+++ src/js/sc.js
@@ -1,29 +1,13 @@
 import {client_id} from "./credentials";
+import $ from 'jQuery';
+import _ from 'lodash';
 
-var baseURL = "https://api.soundcloud.com";
+var baseURL = `https://api.soundcloud.com`;
 
-SC.initialize({
-  client_id: client_id
-});
+function musicSearch(input){
+	return $.ajax({
+		url: `${baseURL}/tracks?q=${input}&client_id=${client_id}`
+	});
+};
 
-// stream track id 293
-SC.stream('/tracks/293').then(function(player){
-  player.play();
-});
-
-// SC.initialize({
-//   client_id: 'YOUR_CLIENT_ID'
-// });
-
-// // find all sounds of buskers licensed under 'creative commons share alike'
-// SC.get('/tracks', {
-//   q: 'buskers', license: 'cc-by-sa'
-// }).then(function(tracks) {
-//   console.log(tracks);
-// });
-
-// 
-
-// id, title, user.username, user.permalink_url, user.avatar_url, artwork_url
-
-export {baseURL};
+export {baseURL, musicSearch};

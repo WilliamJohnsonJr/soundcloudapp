d927c79842834b00e0fafd1949f4301011180d60
Will Johnson <williamterryjohnsonjr@gmail.com>
2016-07-01 16:05:43 -0400

    Added a .on() listener on the JS-generated image links to insert an <audio> tag in index.html with a src attribute containing the value of the baseURL along with  the id of the corresponding track

--- src/js/main.js
+++ src/js/main.js
@@ -7,18 +7,25 @@ $(".searchButton").on('click', function (e) {
 	e.preventDefault();
 	var input = $(".searchInput").val();
 	var musicData = musicSearch(input);
-
+	console.log(musicData);
 	musicData.then(function(musicData){
 		musicData.forEach(function(object){
 			$(".grid").append(`
 				<div class="trackContainer">
 					<div class="trackArtworkDiv">
-						<img class="artwork" src="${object.artwork_url}">
+						<a id=${object.id} href="#" class="imgLink"><img class="artwork" src="${object.artwork_url}"></a>
 					</div>
 					<div class="songTitle">${object.title}
 					</div>
 					<div class="bandName">${object.user.username}
 				</div>`);
 		});
+			$(".imgLink").on('click', function(e){
+				e.preventDefault();
+				$(".audioDiv").html(`
+					 <audio class="audioTag" src="${baseURL}/tracks/${e.currentTarget.id}/stream?client_id=${client_id}" controls autoplay>
+			        </audio>`);
+			});
 	});
 });
+

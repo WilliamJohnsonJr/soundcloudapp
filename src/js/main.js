7b7e9bcce5d1a28665f1a430d6e54c3e5b7019c8
Will Johnson <williamterryjohnsonjr@gmail.com>
2016-07-02 18:51:03 -0400

    Added .append() method to code in second Event Listener in main.js to add the song title to a Now Playing div in index.htmlwhen user clicks on track artwork. Removed all existing code in main.scss

--- src/js/main.js
+++ src/js/main.js
@@ -10,6 +10,10 @@ $(".searchButton").on('click', function (e) {
 	console.log(musicData);
 	musicData.then(function(musicData){
 		musicData.forEach(function(object){
+			if (object.artwork_url === null) {
+				object.artwork_url = "http://placekitten.com/100/100";
+			};
+
 			$(".grid").append(`
 				<div class="trackContainer">
 					<div class="trackArtworkDiv">
@@ -20,12 +24,23 @@ $(".searchButton").on('click', function (e) {
 					<div class="bandName">${object.user.username}
 				</div>`);
 		});
-			$(".imgLink").on('click', function(e){
-				e.preventDefault();
-				$(".audioDiv").html(`
-					 <audio class="audioTag" src="${baseURL}/tracks/${e.currentTarget.id}/stream?client_id=${client_id}" controls autoplay>
-			        </audio>`);
+		$(".imgLink").on('click', function(e){
+			e.preventDefault();
+			var songID = e.currentTarget.id;
+			$(".audioDiv").html(`
+				 <audio class="audioTag" src="${baseURL}/tracks/${e.currentTarget.id}/stream?client_id=${client_id}" controls autoplay>
+		        </audio>`);
+			var songTitleArray = [];
+			musicData.forEach(function(object){
+				if (object.id == songID){
+					songTitleArray.push(object.title);
+				};
 			});
+			$(".audioDiv").append(`
+				<div class="nowPlaying">Now Playing: ${songTitleArray[0]}
+				</div>`);
+		});
 	});
 });
 
+			

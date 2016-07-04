23a5f65b1d407b5f2552c5d71b02bacc3976aa92
Will Johnson <williamterryjohnsonjr@gmail.com>
2016-07-04 00:39:19 -0400

    Added SoundCloud logos in accordance with the SC API terms of use, added links to User pages, and to track permalink URLs, styled in SASS

--- src/js/main.js
+++ src/js/main.js
@@ -7,23 +7,21 @@ $(".searchButton").on('click', function (e) {
 	e.preventDefault();
 	var input = $(".searchInput").val();
 	var musicData = musicSearch(input);
+	console.log(musicData);
+	$(".grid").html("");
 	musicData.then(function(musicData){
-		$(".container").append(`
-			<div class="searchResultsDiv">Search Results:
-      		</div>
-		    <div class="grid">
-		    </div>`);
 		musicData.forEach(function(object){
 			if (object.artwork_url === null) {
 				object.artwork_url = "http://placehold.it/100x100?text=No+Image+Found";
 			};
-
 			$(".grid").append(`
 				<div class="trackContainer">
 					<div class="trackArtworkDiv">
 						<a id=${object.id} href="#" class="imgLink"><img class="artwork" src="${object.artwork_url}"></a>
 					</div>
 					<div class="songTitle">${object.title}
+					</div>
+					<div class="userName"><a class="userNameLink" href="${object.user.uri}">${object.user.username}</a>
 					</div>`);
 		});
 		$(".imgLink").on('click', function(e){
@@ -33,13 +31,24 @@ $(".searchButton").on('click', function (e) {
 				 <audio class="audioTag" src="${baseURL}/tracks/${e.currentTarget.id}/stream?client_id=${client_id}" controls autoplay>
 		        </audio>`);
 			var songTitleArray = [];
+			var musicSourceArray = [];
 			musicData.forEach(function(object){
 				if (object.id == songID){
 					songTitleArray.push(object.title);
 				};
 			});
-			$(".audioDiv").append(`
+			musicData.forEach(function(object){
+				if (object.id == songID){
+					musicSourceArray.push(object.permalink_url)
+				};
+			});
+			$(".audioDiv").append(`<div class="scLinkDiv">
+				<a class="scLink" href="${musicSourceArray[0]}"><img class="scLinkBigImg" src="./images/BigBlackSCLogo.png"></a>
+				</div>
 				<div class="nowPlaying">Now Playing: ${songTitleArray[0]}
+				</div>
+				<div class="scLinkDiv2">
+					<a class="scLink2" href="${musicSourceArray[0]}"><img class="scLinkImg" src="./images/blackSCLogo.png"></a>
 				</div>`);
 		});
 	});
